fe76b59bdb72c8fae17379ac5a74881d296e3f94
DoongE <50088529+ZzangWoo@users.noreply.github.com>
2024-05-21 22:59:18 +0900

    로그인 div 동적 변경 테스트 중

--- src/main/java/com/idiots/openapi/controller/ViewController.java
+++ src/main/java/com/idiots/openapi/controller/ViewController.java
@@ -1,14 +1,21 @@
 package com.idiots.openapi.controller;
 
 
+import com.idiots.openapi.dto.UserResponseDto;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Enumeration;
 
 @Controller
 public class ViewController {
 
     @GetMapping("/")
-    public String home() {
+    public String home()
+    {
         return "views/index";
     }
 
@@ -18,7 +25,15 @@ public class ViewController {
     }
 
     @GetMapping("/signin")
-    public String signin() {
-        return "views/signin";
+    public String signin(HttpSession session) {
+        session.setAttribute("loginUser", "Test");
+        return "redirect:/";
+//        return "views/signin";
+    }
+
+    @GetMapping("/signout")
+    public String signout(HttpSession session) {
+        session.removeAttribute("loginUser");
+        return "redirect:/";
     }
 }
