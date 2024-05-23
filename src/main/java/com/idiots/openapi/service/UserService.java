15c4b1f14a11fedbb97e366b1d28f9c3ad9f4f13
건부기 <drybugi@gmail.com>
2024-05-23 18:12:34 +0900

    Feature/signup (#17)
    * Feat: create signup page (~ing)
    
    * Feat: signup page(password validate)
    
    * Fix: 회원가입 오류 수정
    
    * Fix: fix signup validate
    
    * Feat: apply sign up (~ing)

--- src/main/java/com/idiots/openapi/service/UserService.java
+++ src/main/java/com/idiots/openapi/service/UserService.java
@@ -1,11 +1,17 @@
 package com.idiots.openapi.service;
 
+import com.idiots.openapi.dto.UserRequestDto;
 import com.idiots.openapi.dto.UserResponseDto;
+import com.idiots.openapi.entity.User;
+import com.idiots.openapi.exception.BaseExceptionStatus;
+import com.idiots.openapi.exception.Exception400;
 import com.idiots.openapi.exception.Exception404;
+import com.idiots.openapi.exception.Exception500;
 import com.idiots.openapi.exception.user.UserExceptionStatus;
 import com.idiots.openapi.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,6 +23,8 @@ import java.util.List;
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder bCryptPasswordEncoder;
+
     public List<UserResponseDto> findAll() {
         return userRepository.findAll()
                 .stream()
@@ -30,6 +38,30 @@ public class UserService {
                         .findById(userId)
                         .orElseThrow(() -> new Exception404(UserExceptionStatus.USER_NOT_FOUND))
         );
+    }
+
+    // 중복 검사 체크
+    public void validateDuplicateEmail(String email) {
+        if(userRepository.existsByEmail(email)){
+            throw new Exception400(UserExceptionStatus.USER_ALREADY_EXIST);
+        }
+    }
+
+    // 회원가입
+    @Transactional
+    public UserResponseDto create(UserRequestDto userRequestDto) {
+        validateDuplicateEmail(userRequestDto.email());
+        User user = userRequestDto.toEntity();
+        user.hashPassword(bCryptPasswordEncoder);
+
+        try {
+            userRepository.save(user);
+            return UserResponseDto.of(user);
+        } catch(Exception e) {
+            e.printStackTrace();
+            throw new Exception500(UserExceptionStatus.USER_SAVE_ERROR);
+        }
+
     }
 
 }
