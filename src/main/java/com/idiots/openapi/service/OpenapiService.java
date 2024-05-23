d7ce96ddac23c073fa555ad2364d683bef830907
chang-lis <cwcho@labgenomics.com>
2024-05-23 10:51:04 +0900

    API Config 사용 변경 및 BizM 서비스 추가

--- src/main/java/com/idiots/openapi/service/OpenapiService.java
+++ src/main/java/com/idiots/openapi/service/OpenapiService.java
@@ -9,18 +9,26 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 @Service
 public class OpenapiService {
+    @Value("${openapi.host}")
+    private String host;
 
-    private final WebClient webClient;
+    @Value("${openapi.endPoint}")
+    private String endPoint;
 
     @Value("${openapi.serviceKey}")
     private String SERVICE_KEY;
 
+    private final WebClient webClient;
+
     public Mono<String> getOpenapi() {
         return webClient.get()
                 .uri(uriBuilder -> uriBuilder
+                        .scheme("http")
+                        .host(host)
+                        .path(endPoint)
                         .queryParam("serviceKey", SERVICE_KEY)
                         .queryParam("dataType", "JSON")
-                        .queryParam("base_date", "20240519")
+                        .queryParam("base_date", "20240523")
                         .queryParam("base_time", "0630")
                         .queryParam("nx", 120)
                         .queryParam("ny", 60)
